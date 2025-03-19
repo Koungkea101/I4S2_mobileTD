f68f4c1cc02f197168d18046f4b3776528ea633b
Nann Koungkea <nannkoungkea00@gmail.com>
2025-03-19 23:01:40 +0700

    create portfolio profile

---
+++ android/app/src/main/kotlin/com/example/myfirst_mobileproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.myfirst_mobileproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment 1 portfolio',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const PortfolioPage(),
+    );
+  }
+}
+
+class PortfolioPage extends StatelessWidget {
+  const PortfolioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              //Logo and menu
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.class_, color: Colors.blue, size: 40),
+                      const SizedBox(width: 8),
+                      const Text(
+                        'I4-GICB',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
+                ],
+              ),
+              // const SizedBox(height: 10),
+
+              //profile
+              ClipOval(
+                child: Container(
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                    image: DecorationImage(
+                      image: AssetImage('assets/profile.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                    border: Border.all(color: Colors.grey.shade200, width: 5),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10), //gap
+              //welcome text
+              const Text(
+                'Welcome to my Portfolio Page',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 10),
+
+              const Text(
+                "Hi I'm",
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+              ),
+              // const SizedBox(height: 20),
+
+              //name
+              const Text(
+                "Nann Koungkea",
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),
+              ),
+
+              //role
+              const Text(
+                'Engineering Student',
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+
+              //description
+              const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+
+              //button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder:
+                          (context) => AlertDialog(
+                            title: const Text("Alert"),
+                            content: const Text("Thanks you for hiring me!"),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text('Hire Me!', style: TextStyle(fontSize: 18)),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder:
+                          (context) => AlertDialog(
+                            title: const Text("Alert"),
+                            content: const Text("Downloading.....!"),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          ),
+                    );
+                  },
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(color: Colors.blue),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Download CV',
+                        style: TextStyle(fontSize: 18, color: Colors.blue),
+                      ),
+                      SizedBox(width: 8),
+                      Icon(Icons.download, color: Colors.blue),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
